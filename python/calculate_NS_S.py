b6c6b006c072b7f7f7156c694f762b5c0efff162
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2021-10-07 10:50:26 +0200

    improves implementation of N and S and convert result to fractions

--- python/calculate_NS_S.py
+++ python/calculate_NS_S.py
@@ -4,47 +4,58 @@ import re
 from typing import Tuple
 import pandas as pd
 from Bio import SeqIO
-from Bio.Seq import Seq
 from Bio.codonalign.codonseq import *
 from Bio.codonalign import codonseq
 from Bio.Data import CodonTable
 
 
-def calculate_ns_s(sequence: Seq) -> Tuple[int, int]:
+def calculate_ns_s(sequence: Seq) -> Tuple[float, float]:
+    """
+    returns
+    - the fraction of non-synonymous mutations across all positions in the sequence
+    - the fraction of synonymous mutations across all positions in the sequence
+    """
 
     # sanity checks
     assert len(sequence) % 3 == 0, "The sequence must be a multiple of 3"
     for b in sequence:
         assert b in ["A", "C", "G", "T"], "The sequence contains other bases than A, C, G, T: {}".format(b)
 
+    possible_mutations = {
+        "A": ["C", "G", "T"],
+        "C": ["A", "G", "T"],
+        "G": ["C", "A", "T"],
+        "T": ["C", "G", "A"],
+    }
+
     # translate the sequence
-    protein = sequence.translate()
     ns = 0
-    s = 0
 
     # introduce all possible point mutations
     for i in range(0, len(sequence), 3):
         codon = sequence[i:i + 3]
-        sequence_start = sequence[0:i]
-        sequence_end = sequence[i + 3:]
+        aminoacid = codon.translate()
         for j in range(3):
             base = codon[j]
-            for k in {"A", "C", "G", "T"}.difference(base):
+            for k in possible_mutations.get(base):
                 # mutation k in codon position j in ith codon within sequence
-                new_codon = codon[0:j] + k + codon[j+1:]
-                new_sequence = sequence_start + new_codon + sequence_end
-                new_protein = new_sequence.translate()
-                if new_protein == protein:
-                    # counts a synonymous mutation if the translated sequence does not change
-                    s += 1
-                else:
+                new_codon = codon[0:j] + k + codon[j + 1:]
+                new_aminoacid = new_codon.translate()
+                if new_aminoacid != aminoacid:
                     # counts a non synonymous mutation if the translated sequence changes
                     ns += 1
 
-    return ns, s
+    ns = ns / 3.0   # converts the count to the fraction of all possible mutations in a position (ie: 3)
+    return ns, len(sequence) - ns
 
 
 def calculate_ns_s_2(sequence: Seq) -> Tuple[int, int]:
+    """
+    this implementation is based on existing code in the biopython library
+    the results are equivalent to the above with two exceptions:
+    1. the stop codon is not considered
+    2. this implementation is way faster
+    """
     codon_sequence = CodonSeq(str(sequence))
     s, ns = codonseq._count_site_NG86(
         codon_lst=codonseq._get_codon_list(codon_sequence)[0:-1],
@@ -60,14 +71,14 @@ def calculate_ns_s_over_genes():
         for record in SeqIO.parse(handle, "fasta"):
             transcript = re.sub("\\..*", "", record.id)
             gene = re.compile(r'.*gene_symbol:(\w+)').match(record.description).group(1)
-            ns, s = calculate_ns_s_2(sequence=record.seq)
+            ns, s = calculate_ns_s(sequence=record.seq)
             data.get("transcript").append(transcript)
             data.get("gene").append(gene)
             data.get("NS").append(ns)
             data.get("S").append(s)
 
     df = pd.DataFrame(data=data)
-    df.to_csv("../references/genes_NS_S_2.csv", index=False)
+    df.to_csv("../references/genes_NS_S.csv", index=False)
 
 
 calculate_ns_s_over_genes()
@@ -100,13 +111,13 @@ def calculate_ns_s_over_domains():
             # convert here 1-based protein coordinate to 0-based DNA coordinates
             start = (int(d.get('start')) - 1) * 3
             end = (int(d.get('end')) - 1) * 3
-            ns, s = calculate_ns_s_2(transcript_sequence[start:end])
+            ns, s = calculate_ns_s(transcript_sequence[start:end])
             domains.get("NS").append(ns)
             domains.get("S").append(s)
             domains.get("start").append(start)
             domains.get("end").append(end)
     df = pd.DataFrame(data=domains)
-    df.to_csv("../references/domains_NS_S_2.csv", index=False)
+    df.to_csv("../references/domains_NS_S.csv", index=False)
 
 
 calculate_ns_s_over_domains()
